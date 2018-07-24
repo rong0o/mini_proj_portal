f7b8798a6289c7232652317ccece61b8c7203b5e
lhyt <515593899@qq.com>
2018-07-24 23:15:34 +0800

    Update recording.js

--- pages/recording/recording.js
+++ pages/recording/recording.js
@@ -1,18 +1,44 @@
-// pages/myPage/myPage.js
+/**
+ * 录音模块
+ * create by wooyhe
+ */
+const recorderManager = wx.getRecorderManager()
+const innerAudioContexts = [] //存放音频上下文
+const urls = [] //存放录音微信文件的路径
+const recorderManagers = [] //存放录音机
+const textUrl = 'http://e377706c.ngrok.io' //线上地址
+let innerVideoContext, isrecording, resurl
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    urls: '',
+    list: [
+      { sentence: 'hello', chinese: '你好', slider2change:0, currentTime: 50, duration: 3},//假数据
+      { sentence: 'you', chinese: '你', slider2change: 0, currentTime: 80, duration: 3 }
+    ],
+    recording:{
+      state:false,
+      currentTime:0,
+      duration: 0,
+      index:0
+    },
+    name: 10,
+    status: true,
+    isdisabled: true
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    //视频相关初始化配置
+    innerVideoContext = wx.createVideoContext('myVideo', this) 
+    innerVideoContext.seek(0)
+    this.data.list.forEach(x=>{
+      recorderManagers.push(wx.getRecorderManager())
+    })
   },
 
   /**
