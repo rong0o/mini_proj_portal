af00870649fc580454e759fdc1ff93466d92c147
ciccyliu <ciccyliu@tencent.com>
2018-07-28 15:15:21 +0800

    userPlayPage 接口80%

--- pages/userPlayPage/userPlayPage.js
+++ pages/userPlayPage/userPlayPage.js
@@ -1,4 +1,8 @@
 // pages/userPlayPage/userPlayPage.js
+var app = getApp();
+var host = app.globalData.host;
+var token = app.globalData.token;
+var userId = app.globalData.id;
 Page({
 
   /**
@@ -7,22 +11,18 @@ Page({
   data: {
     comment: [],
     audioId: 0,
-    power: 100,
-    charisma: 100,
     isLike: false,
     isCollect: false,
     collect_text: "收藏",
     dianzan_color: "#bfbfbf",
     isCommentIput: false,
-    vedioInfo: {
-      vedioScr: "http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400",
-      audioScr: "",
-      like: "",
-      score: "",
-      username: "",
-      userImage: 'http://ojg9qyra6.bkt.clouddn.com/1.png',
-      recordDate: ""
-    }
+    vedioScr: "http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400",
+    audioScr: "",
+    charisma: "",
+    power: "",
+    username: "",
+    userImage: 'http://ojg9qyra6.bkt.clouddn.com/1.png',
+    recordDate: ""
   },
   onTapInputComment: function () {
     var isCommentIput = this.data.isCommentIput;
@@ -31,34 +31,51 @@ Page({
     });
   },
   onCommentSubmit: function (e) {
-    //发送评论
-    wx.request({
-      url: host + "audioDetail",
-      method: "POST",
-      header: {
-        'content-type': 'application/json'
-      },
-      success: (res) => {
-        if (res.data.error_code == 0) {
-          this.setData({
-            vedioInfo: res.data.data
-          });
-          console.log(res.data.data);
-        } else
-          console.log("ERROR:error_code" + res.data.error_code);
-      },
-      fail: () => {
-        console.log("vedio data error");
-      }
+    this.setData({
+      isCommentIput: !this.data.isCommentIput
     });
+    console.log(e.detail)
+    //发送评论
+    var url = host + "/commitComment";
+    var data = {
+      audioId: audioId,
+      userId: userId,
+      comment: e.target.value.comment,
+      num: 1,
+      token: token,
+    };
+    var callback = () => {
+      this.setData({
+        comment: res.data.data
+      });
+      console.log(res.data.data);
+    }
+    this.ajax(url, data, callback);
   },
+  //点赞
   onTapLike: function (e) {
     var isLike = this.data.isLike;
     this.setData({
       isLike: !isLike
     });
+    var like_type = !isLike ? 1 : 2;
+    var url = host + "/audioLike";
+    var data = {
+      audioId: audioId,
+      userId: userId,
+      type: like_type,
+      token: token,
+    };
+    var callback = () => {
+      this.setData({
+        charisma: res.data.data.charm
+      });
+      console.log(res.data.data);
+    }
+    this.ajax(url, data, callback);
 
   },
+  //收藏
   onTapCollect: function (e) {
     var isCollect = !this.data.isCollect;
     var collectText = isCollect ? "取消收藏" : "收藏";
@@ -66,9 +83,23 @@ Page({
       isCollect: isCollect,
       collect_text: collectText
     });
-
+    var collect_type = !isCollect ? 1 : 2;
+    var url = host + "/collection_audio";
+    var data = {
+      audioId: audioId,
+      userId: userId,
+      type: like_type,
+      token: token,
+    };
+    var callback = () => {
+    }
+    this.ajax(url, data, callback);
   },
   onTapToRecording: () => {
+
+  },
+  //刷新评论列表
+  onScrollCommentList: function () {
 
   },
   /**
@@ -79,32 +110,82 @@ Page({
       audioId: options.audioId
     })
     //请求视频信息
+    var url = host + "/audioDetail";
+    var data = {};
+    var callback = () => {
+      this.setData({
+        vedioScr: this.data.data.vedioScr,
+        audioScr: this.data.data.audioScr,
+        charisma: this.data.data.like,
+        power: this.data.data.score,
+        username: this.data.data.username,
+        userImage: this.data.data.userImage,
+        recordDate: this.data.data.recordDate
+      });
+      console.log(res.data.data);
+    }
+    this.ajax(url, data, callback)
+
+    //请求评论列表
+    this.reqCommit(1, 1);
+
+    //查询点赞
+    var url = host + "/audioLike";
+    var data = {
+      audioId: audioId,
+      userId: userId,
+      type: 0,
+      token: token,
+    };
+    var callback = () => {
+      this.setData({
+        isLike: res.data.data.like
+      });
+      console.log(res.data.data);
+    }
+    this.ajax(url, data, callback);
+    //查询收藏
+    var url = host + "/collection_audio";
+    var data = {
+      audioId: audioId,
+      userId: userId,
+      type: like_type,
+      token: token,
+    };
+    var callback = (res) => {
+      var isCollect = res.data.data.collect;
+      var collectText = isCollect ? "取消收藏" : "收藏";
+      this.setData({
+        isCollect: isCollect,
+        collect_text: collectText
+      });
+    }
+    this.ajax(url, data, callback);
+  },
+
+
+  //请求函数：
+  ajax: function (url, data, callback) {
     wx.request({
-      url: host + "audioDetail",
+      url: host + "/audioDetail",
+      data: data,
       method: "POST",
       header: {
         'content-type': 'application/json'
       },
-      success: (res) => {
-        if (res.data.error_code == 0) {
-          this.setData({
-            vedioInfo: res.data.data
-          });
-          console.log(res.data.data);
-        } else
-          console.log("ERROR:error_code" + res.data.error_code);
+      success: function () {
+        callback && callback();
       },
-      fail: () => {
-        console.log("vedio data error");
+      fail: function () {
+        console.log(url + "data error");
       }
     });
-    //请求评论列表
-
   },
+  //评论请求函数
   reqCommit: function (page, audioId) {
     wx.request({
-      url: host + "comment",
-      method: "POST",
+      url: host + "/comment",
+      method: "GET",//"POST",
       data: {
         page: page,
         num: 8,
@@ -114,13 +195,10 @@ Page({
         'content-type': 'application/json'
       },
       success: (res) => {
-        if (res.data.error_code == 0) {
-          this.setData({
-            comment: res.data.data
-          });
-          console.log(res.data.data);
-        } else
-          console.log("ERROR:error_code" + res.data.error_code);
+        this.setData({
+          comment: res.data.data
+        });
+        console.log(res.data.data);
       },
       fail: () => {
         console.log("comment data error");
