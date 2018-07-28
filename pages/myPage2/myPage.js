753672b606c09a89375c79c62514683f967e4ed1
xuanhaoguo(郭轩豪) <xuanhaoguo@tencent.com>
2018-07-28 13:15:30 +0800

    add id

--- pages/myPage2/myPage.js
+++ pages/myPage2/myPage.js
@@ -1,47 +1,79 @@
-// pages/myPage/myPage.js
-
-var app = getApp()
+﻿// pages/myPage/myPage.js
+var app=getApp();
+var host=app.globalData.host;
 Page({
-
   /**
    * 页面的初始数据
    */
-  data: {
-    userInfo: {}
-  },
-  onLoad: function () {
-    var userInfo = wx.getStorageSync('userInfo')
+  bindGetUserInfo: function (e) {
+    console.log(e.detail.userInfo)
+    try {
+      var nickname = e.detail.userInfo.nickName
+      var avatarurl = e.detail.userInfo.avatarUrl
+      var wechatid = wx.getStorageSync('wechatid')
+      console.log(wechatid)
+      wx.setStorageSync('nickname', e.detail.userInfo.nickName)
+      wx.setStorageSync('avatarurl', e.detail.userInfo.avatarUrl)
+      wx.setStorageSync('userInfo', e.detail.userInfo)
+      wx.request({
+        url: 'http://134.175.160.37:80/user/add', //仅为示例，并非真实的接口地址
+        data: {
+          username: nickname,
+          avatarUrl: avatarurl,
+          wechatid: wechatid,
+          token: wechatid
+        },
+        success: function (res) {
+          app.globalData.id = res.data.id
+        }
+      })
+      this.bindTapToPlayPage()
+      /*wx.navigateTo({
+        url: '../workDetail/workDetail',
+      })*/
+    } catch (e) {
+
+    }
     var that = this
     that.setData({
-      userInfo: userInfo,
+      userInfo: e.detail.userInfo,
+      canIUse: false,
+      getUser: true
     })
   },
-  bindTapToMyWorks: function () {
-    wx.navigateTo({
-      url: '../workDetail/workDetail',
-    })
-  },
-  bindTapMyCollect: function () {
-    wx.navigateTo({
-      url: '../workDetail/workDetail',
-    })
-  },
-  bindTapToMyPower: function () {
-    /*wx.navigateTo({
-      //url: '../myPower/myPower',
-      url: '../StrengthRank/StrengthRank',
-    })*/
+  bindTapToPlayPage:function(){
     wx.switchTab({
-      url: '../StrengthRank/StrengthRank',
+      url: '../index/index',
     })
   },
-  bindTapToMyCharm: function () {
+  //点击更多
+  bindTapToWorkDetail: function (e) {
+    const type=e.target.dataset.type+1;
+    const hot=e.target.dataset.hot;
     wx.navigateTo({
-      //url: '../myCharm/myCharm',
-      url: '../charmRank/charmRank',
+      url: '../workDetail/workDetail?type=' + type+'&hot='+hot,
     })
   },
+  bindTapToSwitchNav:function(e){
+    let index = e.target.dataset.index;
+    this.setData({
+      current_index:index,
+      current_tab:index,
+    })
+  },
+  tabChange:function(e){
+    this.setData({
+      current_index:e.detail.current
+    });
+    console.log(1);
 
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
