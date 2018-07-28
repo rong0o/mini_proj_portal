b94736552de1d65be9d8a0ad8a9d7ae4fe813152
Evan <evanzhengli@tencent.com>
2018-07-28 11:05:37 +0800

    the code of xuanhaoguo

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
+    var that = this
     // 登录
     wx.login({
       success: res => {
@@ -16,8 +16,9 @@ App({
           wx.request({
             url: 'https://api.weixin.qq.com/sns/jscode2session?appid=wx9e946873f852c1cf&secret=a2eb8c22f96238ad991bf929d75806f5&js_code=' + res.code + '&grant_type=authorization_code',
             success: function (res) {
-              console.log(res.data)
-              wx.setStorageSync('wechatid', res.openid)
+              wx.setStorageSync('wechatid', res.data.openid)
+              wx.setStorageSync('token', res.data.openid)
+              that.globalData.token = res.data.openid;
             }
           })
         } else {
@@ -25,30 +26,10 @@ App({
         }
       }
     })
-
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
   },
   globalData: {
     userInfo: null,
-    host: "http://rong.com/"
+    host: "http://134.175.160.37",
+    token:""
   }
 })
--- pages/myPage/myPage.js
+++ pages/myPage/myPage.js
@@ -1,75 +1,157 @@
 // pages/myPage/myPage.js
+
+var app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    userInfo: {}
   },
-  bindTapToMyWorks:function(){
-      wx.navigateTo({
-        url: '../workDetail/workDetail',
+  onLoad: function() {
+    var that = this
+    if (this.getUser == true) {
+      canIUse = false
+    } else {
+      var nickname = wx.getStorageSync('nickname')
+      var avatarurl = wx.getStorageSync('avatarurl')
+      var userInfo = wx.getStorageSync('userInfo')
+      if (nickname != '' && nickname != null) {
+        console.log(nickname)
+        console.log(avatarurl)
+        that.setData({
+          userInfo: userInfo,
+          canIUse: false,
+          getUser: true
+        })
+      } else {
+        wx.getSetting({
+          success: function(res) {
+            if (res.authSetting['scope.userInfo']) {
+              // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+              wx.getUserInfo({
+                success: function(res) {
+                  console.log(res.userInfo)
+                  console.log(res.encryptedData)
+
+                  that.setData({
+                    userInfo: res.userInfo,
+                    canIUse: false,
+                    getUser: true
+                  })
+                }
+              })
+            }
+          }
+        })
+      }
+    }
+  },
+
+  bindGetUserInfo: function(e) {
+    console.log(e.detail.userInfo)
+    try {
+      var nickname = e.detail.userInfo.nickName
+      var avatarurl = e.detail.userInfo.avatarUrl
+      var wechatid = wx.getStorageSync('wechatid')
+      
+      wx.setStorageSync('nickname', e.detail.userInfo.nickName)
+      wx.setStorageSync('avatarurl', e.detail.userInfo.avatarUrl)
+      wx.setStorageSync('userInfo', e.detail.userInfo)
+
+      wx.request({
+        url: 'localhost:/user/add', //仅为示例，并非真实的接口地址
+        data: {
+          nickname: nickname,
+          avatarurl: avatarurl,
+          wechatid: wechatid
+        },
       })
+    } catch (e) {
+
+    }
+    var that = this
+    that.setData({
+      userInfo: e.detail.userInfo,
+      canIUse: false,
+      getUser: true
+    })
   },
-  bindTapMyCollect:function(){
-      wx.navigateTo({
-        url: '../workDetail/workDetail',
-      })
+  bindTapToMyWorks: function() {
+    wx.navigateTo({
+      url: '../workDetail/workDetail',
+    })
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  bindTapMyCollect: function() {
+    wx.navigateTo({
+      url: '../workDetail/workDetail',
+    })
   },
+  bindTapToMyPower: function () {
+    /*wx.navigateTo({
+      //url: '../myPower/myPower',
+      url: '../StrengthRank/StrengthRank',
+    })*/
+    wx.switchTab({
+      url: '../StrengthRank/StrengthRank',
+    })
+  },
+  bindTapToMyCharm: function () {
+    wx.navigateTo({
+      //url: '../myCharm/myCharm',
+      url: '../charmRank/charmRank',
+    })
+  },
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
   }
 })
