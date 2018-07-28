241586c173279f1f51a5ce6370d2ac01b3b6d65f
gxh27954 <756605998@qq.com>
2018-07-28 20:07:39 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 //app.js
 App({
   onLaunch: function () {
+    wx.setEnableDebug({
+      enableDebug: true
+    })
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
